8d70604445314e25555b0ffcffed6130046f9df1
cwj851 <53599391+cwj851@users.noreply.github.com>
2023-07-22 23:19:27 +0800

    update

--- app/src/main/java/pansong291/xposed/quickenergy/util/FriendIdMap.java
+++ app/src/main/java/pansong291/xposed/quickenergy/util/FriendIdMap.java
@@ -76,7 +76,7 @@ public class FriendIdMap {
         for (Map.Entry<String, String> entry : idSet) {
             if (entry.getValue().split("\\|").length < 1) {
                 idList.add(entry.getKey());
-                Log.i(TAG, "未知id: " + entry.getKey());
+                //Log.i(TAG, "未知id: " + entry.getKey());
             }
         }
         return idList;
@@ -91,7 +91,7 @@ public class FriendIdMap {
                 try {
                     String[] idSet = str.split("\n");
                     for (String s : idSet) {
-                        Log.i(TAG, s);
+                        //Log.i(TAG, s);
                         int ind = s.indexOf(":");
                         idMap.put(s.substring(0, ind), s.substring(ind + 1));
                     }
